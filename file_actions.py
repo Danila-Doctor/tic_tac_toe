d44bfe29cbc40790404ca8ef0fddbdb0b55f8ae3
Danila <KorshunovDanila12@yandex.ru>
2026-03-04 14:59:48 +0300

    fox2

--- file_actions.py
+++ file_actions.py
@@ -1,8 +1,9 @@
-# Открыть файл example.txt на чтение (аргумент 'r').
-file = open('example.txt', 'r', encoding='utf-8')
-# Прочитать первые 12 символов из файла и сохранить их в переменную content.
-content = file.read(12)
-# Вывести на печать содержимое переменной content.
-print(content)
+file = open('example.txt', 'w')
+# Записать в файл строку.
+file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
 # Закрыть файл.
-file.close()
+file = open('example.txt', 'w')
+# Записать в файл строку.
+file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
+# Закрыть файл.
+file.close() 
